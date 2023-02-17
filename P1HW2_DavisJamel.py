f1c6e05f94364ce184173f8081f1363db86af89e
Davisja10456 <124644810+Davisja10456@users.noreply.github.com>
2023-02-16 19:18:32 -0500

    P1HW2_DavisJamel Lab
    Nashville Trip Budget

---
+++ P1HW2_DavisJamel.py
@@ -0,0 +1,36 @@
+#Nashville Travel Budget
+#February 16, 2023
+#CTI-110 P1HW2 - Travel Expense
+#Jamel Davis
+
+print('This program calculates and displays travel expenses')
+Budget = 1200
+print('Enter Budget: ', end='')
+print(Budget)
+print('Enter your travel destination: Nashville')
+Gas = 250
+print('How much do you think you will spend on gas? ', end='')
+print(Gas)
+Hotel = 600
+print('Approximately, how much will you need for accomodation/hotel? ', end='')
+print(Hotel)
+Food = 300
+print('Last, how much do you need for food? ', end='')
+print(Food)
+print('------------Travel Expenses------------')
+print('Location: Nashville')
+InitialBudget = 1200
+print('Initial Budget: ', end='')
+print(InitialBudget)
+print ()
+Fuel = 250
+print('Fuel: ', end='')
+print(Fuel)
+Accomodation = 600
+print('Print Accomodation: ', end='')
+print(Accomodation)
+Food = 300
+print('Food: ', end='')
+print(Food)
+print()
+print('Remaining Balance:', InitialBudget - Fuel - Accomodation - Food)
