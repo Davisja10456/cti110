8ab4af1fa99e5c102386b5db7fa0514c5d6daeb4
Davisja10456 <124644810+Davisja10456@users.noreply.github.com>
2023-05-04 11:14:07 -0400

    Shapes

---
+++ P4LAB1a_DavisJamel.py
@@ -0,0 +1,38 @@
+#CTI-110
+#P4LAB1a 
+#Jamel Davis
+#March 29, 2023
+
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("white")
+
+tri = turtle.Turtle()
+tri.color("black")
+tri.pensize(5)
+
+sqr = turtle.Turtle()
+sqr.color("blue")
+sqr.pensize(5)
+
+tri.forward(80)
+tri.left(120)
+tri.forward(80)
+tri.left(120)
+tri.forward(80)
+tri.left(120)
+
+
+sqr.forward(50)
+sqr.left(90)
+sqr.forward(50)
+sqr.left(90)
+sqr.forward(50)
+sqr.left(90)
+sqr.forward(50)
+sqr.left(90)
+
+turtle.done()
+
+
+
